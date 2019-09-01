52d150bdebdc3fd838e5b6cda7672f00cb815ea3
mariaarroyo6215 <mariaarroyo6215@gmail.com>
2019-08-31 22:24:46 -0500

    Practica4 actividad 1,2,extra

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 /* 
 *Autor: Maria Guadalupe Peña Arroyo
 *Fecha: 31/08/2019
-*Grupo: 
+*Grupo: 008
 *Horario: Sabados 9am-11am
 *Matricula: 1868704
 */
@@ -23,14 +23,14 @@ int main(int argc, char *argv[]) {
 	}else{ 
 	printf("el numero %.2f es mayor", num2);
 	}
-	/* Se hace la comparacion con un if/else para sacar el mayor*/
+	/* Se hace la comparacion con un if/else para sacar el mayor y se imprime*/
 	printf("\n\n\n\n\n\n\n");
     system("pause");
 	
 	//*** ACT2
 	
 	int a=5, b=3, c=-12, ver1, ver2, ver3, ver4, ver5 ;
-	
+	/* declaracion de las variables a,b y c; y las variables para hacer operaciones con las anteriores mencionadas*/
 	printf("A>3\n");
 	if(a>3){
 	printf("Verdadero");} else{
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
 	printf("Verdadero");} else{
 		printf("Falso");
 	}
-	
+	/* En cada uno se muestra al usuario lo que se esta comparando para despues mostrar si la sentencia anterior es verdadera o falsa*/
 	printf("\n\n B!=C \n");
 	if(b!=c){
 	printf("Verdadero");} else{
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]) {
 	printf("Verdadero");} else{
 		printf("Falso");
 	}
-	
+	/* se usa una variable distinta a c, b y a  para poder hacer operaciones con estas, cosa que no se puede dentro de la comparaion de nuestro if */
 	ver1=a*b;
 	printf("\n\n A*B=15 \n");
 	if(ver1==15){
@@ -78,7 +78,7 @@ int main(int argc, char *argv[]) {
 	printf("Verdadero");} else{
 		printf("Falso");	
 	}
-	
+	/*se usan mas variables para verificar y hacer operaciones cuyo resultado despues de va a comparar con un valor establecido */
 	ver4= a+b;
 	ver5= a-b;
 	printf("\n\n (A+B = 8) && (A-B = 2) \n");
@@ -98,51 +98,60 @@ int main(int argc, char *argv[]) {
 	printf("Verdadero");} else{
 		printf("Falso");	
 	}
+	/*se hace un paro de sistema y se limpia pantalla para mayor comodidad al usuario*/
 	printf("\n\n\n\n\n\n\n");
     system("pause");
+    system("cls");
+    
 	//*** ACTEXTRA
 	
-    a=0, b=0, c=0;
-	printf("Ingrese un valor entero para A");
-	scanf("d%", &a);  
-	printf("Ingrese un valor entero para B");
-	scanf("d%", &b);
-	printf("Ingrese un valor entero para C");
-	scanf("d%", &c);    
+	/*se pide ingresar valores para a, b y c; se intercala con un fflush ya que sin este el programa no te dejaria ingresar los valores de dichas variables, saltando por completo esta parte del codigo*/
+    int a2, b2, c2;
+    fflush(stdin);
+	printf("Ingrese un valor entero para A: \n");
+	scanf("d%", &a2);
+	fflush(stdin);  
+	printf("Ingrese un valor entero para B: \n");
+	scanf("d%", &b2);
+	fflush(stdin);
+	printf("Ingrese un valor entero para C: \n");
+	scanf("d%", &c2);    
+	fflush(stdin);
 	
+	/*se empiza de nuevo con las comparaciones pero ahora con valores pedidos al usuario*/
 	printf("A>3\n");
-	if(a>3){
+	if(a2>3){
 	printf("Verdadero");} else{
 		printf("Falso");
     }  
 	printf("\n\n A>C \n");
-	if(a>c){
+	if(a2>c2){
 	printf("Verdadero");} else{
 		printf("Falso");
 	}
 	
 	printf("\n\n B!=C \n");
-	if(b!=c){
+	if(b2!=c2){
 	printf("Verdadero");} else{
 		printf("Falso");
 	}
 	
 	printf("\n\n A=3 \n");
-	if(a==3){
+	if(a2==3){
 	printf("Verdadero");} else{
 		printf("Falso");
 	}
 	
-	ver1=a*b;
+	ver1=a2*b2;
 	printf("\n\n A*B=15 \n");
 	if(ver1==15){
 	printf("Verdadero");} else{
 		printf("Falso");
 	}
 	
-	ver2=c/b;
+	ver2=c2/b2;
 	printf("\n\n C/B<A \n");
-	if(ver2<a){
+	if(ver2<a2){
 	printf("Verdadero");} else{
 		printf("Falso");
     }
@@ -151,21 +160,21 @@ int main(int argc, char *argv[]) {
 	printf("Verdadero");} else{
 		printf("Falso");
     }
-	ver3= a+b+c;
+	ver3= a2+b2+c2;
 	printf("\n\n A+B+C=5 \n");
 	if(ver3==5){
 	printf("Verdadero");} else{
 		printf("Falso");	
 	}
 	
-	ver4= a+b;
-	ver5= a-b;
+	ver4= a2+b2;
+	ver5= a2-b2;
 	printf("\n\n (A+B = 8) && (A-B = 2) \n");
 	if(ver4==8 && ver5==2){
 	printf("Verdadero");} else{
 		printf("Falso");	
 	}
-	
+	/*se usan variables para verificar y hacer las peraciones que despues se comparan a distintos valores*/
 	printf("\n\n (A+B = 8) || (A-B = 6) \n");
 	if(ver4==8 || ver5==6){
 	printf("Verdadero");} else{
@@ -173,7 +182,7 @@ int main(int argc, char *argv[]) {
 	}
 	
 	printf("\n\n A>3 && B>3 && C>3 \n");
-	if(a>3 && b>3 && c>3){
+	if(a2>3 && b2>3 && c2>3){
 	printf("Verdadero");} else{
 		printf("Falso");	
 	} 
