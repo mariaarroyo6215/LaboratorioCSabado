f2fca995e64deef6694c9184ac9998baa3e6745e
Maria Arroyo <mariaarroyo6215@gmail.com>
2019-09-14 10:06:17 -0500

    error reinicio

--- main.c
+++ main.c
@@ -3,6 +3,52 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+/*
+*Autor: Maria Guadalupe Peña Arroyo
+*Fecha: 14/09/2019
+*Grupo: 008
+*Horario: Sabados 9-11 am
+*Matricula: 1868704
+*/
+
+
 int main(int argc, char *argv[]) {
+	
+//**************ACTIVIDAD1*****************
+char ver;
+int num, sum=0; /* declaracion de variables*/
+    do
+    {
+	printf("\n Ingrese un numero entero:  ");
+    scanf("%d", &num);
+    sum=sum+num;       /* se va haciendo la sumatoria inmediatamente se ingresa el numero*/
+    fflush(stdin);    /* esto evita se repita la siguiente linia de comando */
+       do
+       {
+        printf("\n Desea introducir otro (s/n)?");
+        scanf("%c", &ver);
+        fflush(stdin);      /* esto evita se repita la siguiente linia de comando */
+        }while (ver!='n' && ver!='s' );
+    }while(ver=='s');       /* sale del ciclo hasta que se ingrese la n*/
+    
+	printf("\n\n La suma de los numeros introducidos es:  %d", sum); /* se imprime sumatoria de numeros deseados*/
+	
+	printf("\n\n\n\n\n\n\n");
+	system("pause");
+	system("cls");    /* se limpia pantalla */
+	
+//**************ACTIVIDAD2*****************
+
+float n;
+    do
+    {
+    printf("\n\n Ingrese un numero: ");
+    scanf("%f", &n);
+    printf("\n\t\t\t\t\ %.2f", n);
+    }while (n<100);
+
+    printf("\n\n\n\n\n\n\n");
+	system("pause");
+	system("cls");
 	return 0;
 }
