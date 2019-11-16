1b122170cca2a9f5bef7771492ee7ada80355e22
mariaarroyo6215 <mariaarroyo6215@gmail.com>
2019-11-16 11:00:33 -0600

    Practica15

--- main.c
+++ main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//void lectura(archivo);
+char invertir(char cad[100]) {
+	int i,j, longitud;
+    char temporal[' '];
+	longitud= strlen(cad);
+    for (i=longitud; i>=0;i--){
+		for (j=0; j<longitud ;j++) {
+			
+			temporal[j]= cad[i];
+		}
+		printf ( "%s" , temporal );
+	}
+	printf("%s", cad);
+  	return cad[100];
+}
 
 int main(int argc, char *argv[]) {
+	FILE *archivo;
+	
+	char cad[100], inv[100];
+	
+	archivo=fopen("archivoOriginal.txt", "w");
+
+	if(archivo == NULL)
+ 	{
+		exit(1);
+	}else{
+		
+		if(archivo!=NULL)
+		{
+			printf("Ingrese una cadena:  ");
+			fflush(stdin);
+			gets(cad);
+			fprintf(archivo, "%s", cad);
+		}
+		
+	}
+	fclose(archivo);
+	
+	invertir(cad);
+
+	printf("\n\n\n");
+	system("pause");
 	return 0;
 }
+
